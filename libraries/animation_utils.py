9e19edad40f5dd4ef1abf683e9e1dc03ee1ba673
Johansmm <johan-steven.mejia-mogollon@imt-atlantique.net>
2021-01-13 23:49:58 -0500

    New tool in 4D spectrogram: Visualization by windows

--- libraries/animation_utils.py
+++ libraries/animation_utils.py
@@ -116,26 +116,28 @@ In:
 Out:
     * Interactif ipywidgets
 """
-def spectogram_anima(graph, signal, kernel = None, SKS = 30, is_graph_space = False,lamdba_lim=None, **kwargs):
+def spectogram_anima(graph, signal, kernel = None, SKS = 30, is_graph_space = False, lamdba_lim = None, **kwargs):
     def update(value, autoadj = False, interpolate = True):
-        nonlocal kwargs , rows_labels
+        nonlocal kwargs, spectrogram, pr_value
         kwargs_save = copy.deepcopy(kwargs)
         if autoadj: kwargs["limits"] = None
         kwargs["interpolate"] = interpolate
-                
-                
+
         # Space-spectogram
-        
-        spectrogram = compute_graph_spectrogram(graph, signal[:, value], kernel)
-        if lamdba_lim is not None :
-            spectrogram = spectrogram[lamdba_lim[0]:lamdba_lim[1]]
-            rows_labels=rows_labels[lamdba_lim[0]:lamdba_lim[1]]
-        plot_matrix(spectrogram, cols_title=cols_title, cols_labels=range(graph.N),
+        if pr_value is None: pr_value = value
+        if spectrogram is None or pr_value != value:
+            spectrogram = compute_graph_spectrogram(graph, signal[:, value], kernel)
+            pr_value = value
+        if lamdba_lim is not None: 
+            plot_matrix(spectrogram[lamdba_lim[0]:lamdba_lim[1]], cols_title=cols_title, cols_labels=range(graph.N),
+                    rows_title="Eigenvalue index", colorbar=True, rows_labels=rows_labels[lamdba_lim[0]:lamdba_lim[1]], 
+                    title=title + str(value), **kwargs)
+        else:
+            plot_matrix(spectrogram, cols_title=cols_title, cols_labels=range(graph.N),
                     rows_title="Eigenvalue index", colorbar=True, rows_labels=rows_labels, 
                     title=title + str(value), **kwargs)
         kwargs = kwargs_save
     
-
     # Slider
     if is_graph_space:
         cols_title = "Vertex"
@@ -154,6 +156,7 @@ def spectogram_anima(graph, signal, kernel = None, SKS = 30, is_graph_space = Fa
     #     layout = ipywidgets.Layout(width='auto'), style = {"handle_color":"lightblue"}, description = desc)}
     params = {"value": ipywidgets.Dropdown(options = range(signal.shape[1]), value = 0, 
             description = desc, disabled=False, style = {"handle_color":"lightblue"},)}
+    spectrogram, pr_value = None, None
     return ipywidgets.widgets.interact(update, **params)    
 
 #############################################################################################################################
@@ -170,34 +173,35 @@ In:
 Out:
     * Interactif ipywidgets
 """
-def JFT_anima(space_time_graph, signal, windows_kernels = None, kernels = None, joint_spectogram = None,lspace_lim=None,ltime_lim=None, **kwargs):
+def JFT_anima(space_time_graph, signal, windows_kernels = None, kernels = None, joint_spectogram = None,
+            lspace_lim = None, ltime_lim = None, **kwargs):
+    cols_labels = ["$\lambda^T_{" + str(x) + "}$" for x in range(space_time_graph[1].N)]
+    rows_labels = ["$\lambda^G_{" + str(x) + "}$" for x in range(space_time_graph[0].N)]
+
     def update(instant, vertex, autoadj = False, interpolate = True) :
         nonlocal kwargs
-        kwargs_save = copy.deepcopy(kwargs)
+        params = copy.deepcopy(kwargs)
         
-        if autoadj: kwargs["limits"] = None
-        elif "limits" not in kwargs.keys(): 
-            kwargs["limits"] = [numpy.min(joint_spectogram), numpy.max(joint_spectogram)]
-        kwargs["interpolate"] = interpolate
-        
-        Copia_JointSpectogram = joint_spectogram.copy()
-        cols_labels=["$\lambda^T_{" + str(x) + "}$" for x in range(space_time_graph[1].N)]
-        rows_labels=["$\lambda^G_{" + str(x) + "}$" for x in range(space_time_graph[0].N)]
+        if autoadj: params["limits"] = None
+        elif "limits" not in params.keys(): 
+            params["limits"] = [numpy.min(joint_spectogram), numpy.max(joint_spectogram)]
+        params["interpolate"] = interpolate
 
-        if lspace_lim is not None :
-            Copia_JointSpectogram = Copia_JointSpectogram[lspace_lim[0]:lspace_lim[1]]
-            rows_labels = rows_labels[lspace_lim[0]:lspace_lim[1]]
-        if ltime_lim is not None :
-            Copia_JointSpectogram = Copia_JointSpectogram[:,ltime_lim[0]:ltime_lim[1]]
-            cols_labels = cols_labels[ltime_lim[0]:ltime_lim[1]]
-        plot_matrix(Copia_JointSpectogram[:,:, vertex, instant],
-                    cols_title="Eigenvalue Time-graph", rows_title="Eigenvalue Vertex-graph", colorbar=True,
-                    cols_labels= cols_labels,
-                    rows_labels= rows_labels ,
-                    title="Graph spectrogram of all values observed at time " + 
-                    str(instant) + " and vertex " + str(vertex), **kwargs)
+        params['cols_title'], params['rows_title'], params['colorbar'] = "Eigenvalue Time-graph", "Eigenvalue Vertex-graph", True
+        params['title'] = "Graph spectrogram of all values observed at time " + str(instant) + " and vertex " + str(vertex)
         
-        kwargs = kwargs_save
+        if lspace_lim is not None and ltime_lim is None:
+            plot_matrix(joint_spectogram[lspace_lim[0]:lspace_lim[1], :, vertex, instant],
+                cols_labels= cols_labels, rows_labels= rows_labels[lspace_lim[0]:lspace_lim[1]], **params)
+        elif lspace_lim is None and ltime_lim is not None:
+            plot_matrix(joint_spectogram[:, ltime_lim[0]:ltime_lim[1], vertex, instant],
+                cols_labels= cols_labels[ltime_lim[0]:ltime_lim[1]], rows_labels= rows_labels, **params)
+        elif lspace_lim is not None and ltime_lim is not None:
+            plot_matrix(joint_spectogram[lspace_lim[0]:lspace_lim[1],ltime_lim[0]:ltime_lim[1], vertex, instant],
+                cols_labels= cols_labels[ltime_lim[0]:ltime_lim[1]], rows_labels= rows_labels[lspace_lim[0]:lspace_lim[1]], **params)
+        else:
+            plot_matrix(joint_spectogram[:,:, vertex, instant], 
+                cols_labels= cols_labels, rows_labels= rows_labels, **params)
     
     # Slider
     if windows_kernels is not None:
@@ -214,11 +218,35 @@ def JFT_anima(space_time_graph, signal, windows_kernels = None, kernels = None, 
     #         "vertex": ipywidgets.IntSlider(min = 0, max = space_time_graph[0].N, step = 1, 
     #     layout = ipywidgets.Layout(width='auto'), style = {"handle_color":"lightblue"})}
 
-    params = {"instant": range(space_time_graph[1].N),
-            "vertex": range(space_time_graph[0].N)}
-
+    params = {"instant": range(space_time_graph[1].N), "vertex": range(space_time_graph[0].N)}
     ipywidgets.widgets.interact(update, **params)
     return kernels, joint_spectogram
+
+#############################################################################################################################
+"""
+Show JFT animation, in a complex graph (space-time graphs), in differents time divisions.
+--
+In:
+    * space_time_graph: List of two PyGSP graph: space(0) and time(1) graphs, in this order.
+    * signal: 2D matrix with signals for each vertex
+    * time_window: Time window to divide all signal in different signals
+    * kwargs: Parameters of 'JFT_anima' function.
+Out:
+    * Interactif ipywidgets
+"""
+def split_JFT_anima(space_time_graph, time_sensors_series, time_window, time_div_default = 0, **kwargs):
+    def update(time_division):
+        nonlocal kwargs
+        kwargs['kernels'], kwargs['joint_spectogram'] = None, None
+        i,j = time_division*time_window, (time_division+1)*time_window
+        kwargs['kernels'], 
+        kwargs['joint_spectogram'] = JFT_anima([space_time_graph[0], create_path_graph(time_window)], 
+                                               time_sensors_series[:,i:j], **kwargs)
+    
+    params = {"time_division": ipywidgets.Dropdown(options = range(space_time_graph[1].N//time_window), 
+        value = time_div_default, description = 'time_division', disabled=False, style = {"handle_color":"lightblue"},)}
+    ipywidgets.widgets.interact(update, **params)
+    return kwargs['kernels'], kwargs['joint_spectogram']
 
 #############################################################################################################################
 ####################################################### Proof functions #####################################################
