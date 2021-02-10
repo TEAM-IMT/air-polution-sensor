c2e9f9a08095f4d1f7a3bb4314d0499fc2694f58
Johansmm <johan-steven.mejia-mogollon@imt-atlantique.net>
2021-02-10 22:06:53 +0100

    First version in cluster implementation

--- libraries/animation_utils.py
+++ libraries/animation_utils.py
@@ -286,38 +286,39 @@ Out:
     * image plot
 """
 
-def dist_matrix_estimation(joint_spectogram, sw, tw, filename = None, overwrite = False, cprint = 100000):
+def dist_matrix_estimation(joint_spectogram, sw, tw, filename = None, overwrite = False, vlist = None, cprint = 100000):
     dist_matrix = pd.DataFrame()
     count, S, T = 0, *joint_spectogram.shape[2:]
     N = S*T
     joint_spectogram = numpy.pad(joint_spectogram, ((0,0),(0,0),(sw,sw),(tw,tw))) # Add zero-pad
     if filename is not None and os.path.isfile(filename) and not overwrite:
-        return pd.read_csv(filename, index_col = 0)
+        dist_matrix = pd.read_csv(filename, index_col = 0)
         # return numpy.loadtxt(filename, delimiter = ',')
-    for a_i in range(S):
-        for a_j in range(T):
-            wspectogram_A = joint_spectogram[:,:, a_i:a_i + 2*sw + 1, a_j:a_j + 2*tw + 1]
-            p1, dist_matrix.loc[p1,p1] = "({},{})".format(a_i,a_j), 0.0 # Distance at the same coordinate = 0
-            for p2 in dist_matrix.index[:-1]:
-                b_i, b_j = eval(p2) # Get center for new spectogram
-                wspectogram_B = joint_spectogram[:,:, b_i:b_i + 2*sw + 1, b_j:b_j + 2*tw + 1]
-                dist_matrix.loc[p1,p2] = numpy.linalg.norm(wspectogram_A - wspectogram_B)
-                dist_matrix.loc[p2,p1] = dist_matrix.loc[p1,p2] # Symmetric matrix
-                if count % cprint == 0: print("[INFO] Matrix processing. {}/{}".format(count + 1, N*(N-1)//2))
-                count += 1        
-    if filename is not None: 
-        # numpy.savetxt(filename, dist_matrix, delimiter = ',')
-        dist_matrix.to_csv(filename)
+    else: 
+        for a_i in range(S):
+            for a_j in range(T):
+                wspectogram_A = joint_spectogram[:,:, a_i:a_i + 2*sw + 1, a_j:a_j + 2*tw + 1]
+                p1, dist_matrix.loc[p1,p1] = "({},{})".format(a_i,a_j), 0.0 # Distance at the same coordinate = 0
+                for p2 in dist_matrix.index[:-1]:
+                    b_i, b_j = eval(p2) # Get center for new spectogram
+                    wspectogram_B = joint_spectogram[:,:, b_i:b_i + 2*sw + 1, b_j:b_j + 2*tw + 1]
+                    dist_matrix.loc[p1,p2] = numpy.linalg.norm(wspectogram_A - wspectogram_B)
+                    dist_matrix.loc[p2,p1] = dist_matrix.loc[p1,p2] # Symmetric matrix
+                    if count % cprint == 0: print("[INFO] Matrix processing. {}/{}".format(count + 1, N*(N-1)//2))
+                    count += 1        
+        if filename is not None or overwrite: 
+            # numpy.savetxt(filename, dist_matrix, delimiter = ',')
+            dist_matrix.to_csv(filename)
+    if vlist is not None:
+        vert_list = ["({},{})".format(i,j) for i in vlist for j in range(joint_spectogram.shape[1])]
+        dist_matrix = dist_matrix.loc[vert_list, vert_list]
     return dist_matrix
 
 def plot_dist_matrix(joint_spectogram, sw, tw, vlist = None, xlim = None, ylim = None, filename = None, 
                     overwrite = False, image_save = None, npair = 200):
     print("[{}][INFO] Start matrix computation.".format(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")))
-    dist_matrix = dist_matrix_estimation(joint_spectogram, sw, tw, filename = filename, overwrite = overwrite)
+    dist_matrix = dist_matrix_estimation(joint_spectogram, sw, tw, filename = filename, overwrite = overwrite, vlist = vlist)
     print("[{}][INFO] Start matrix graph.".format(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")))
-    if vlist is not None:
-        vert_list = ["({},{})".format(i,j) for i in vlist for j in range(joint_spectogram.shape[1])]
-        dist_matrix = dist_matrix.loc[vert_list, vert_list]
     xlabel = "" if xlim is None or (xlim[1] - xlim[0]) > npair else ["$p_{"+x+"}$" for x in dist_matrix.columns[xlim[0]:xlim[1]]]
     ylabel = "" if xlim is None or (ylim[1] - ylim[0]) > npair else ["$p_{"+x+"}$" for x in dist_matrix.index[xlim[0]:xlim[1]]]
     if xlabel == "" and len(dist_matrix.columns) <= npair: xlabel = ["$p_{"+x+"}$" for x in dist_matrix.columns]
